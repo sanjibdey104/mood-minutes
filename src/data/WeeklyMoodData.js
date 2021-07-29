81762194431379cf9d6924e6d507d38c6f1cdc5e
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-07-29 19:58:06 +0530

    refactor: code logic adjustments

--- src/data/WeeklyMoodData.js
+++ src/data/WeeklyMoodData.js
@@ -1,29 +1,24 @@
 import dayjs from "dayjs";
 
 export const weeklyMoodData = (moodLogs) => {
-  // set the last seven days
+  // get the last seven days
   let lastSevenDays = [];
   for (let i = 0; i < 7; i++) {
     let t = new Date();
     t.setDate(t.getDate() - i);
     lastSevenDays.push(t.getDate());
-    // lastSevenDays.push(`${t.getDate()}th`);
   }
-  lastSevenDays = lastSevenDays.sort();
 
-  // update loggedAt timestamp with "DD" equivalent
-  const updatedMoodLogs = moodLogs
-    .map((log) => ({
-      ...log,
-      loggedAt: Number(dayjs(log.loggedAt.toDate()).format("DD")),
-    }))
-    .slice()
-    .sort((a, b) => a.loggedAt - b.loggedAt);
+  // modify loggedAt timestamp with "DD" format to match dates in "lastSevenDays"
+  const modifiedMoodLogs = moodLogs.map((log) => ({
+    ...log,
+    loggedAt: Number(dayjs(log.loggedAt.toDate()).format("DD")),
+  }));
 
-  // match loggedAt day with the corresponding day in last seven days
-  let rawMoodData = lastSevenDays.map((day) =>
-    updatedMoodLogs.find((log) => log.loggedAt === day)
-  );
+  // match loggedAt day with the corresponding day in "lastSevenDays"
+  let rawMoodData = lastSevenDays
+    .sort()
+    .map((day) => modifiedMoodLogs.find((log) => log.loggedAt === day));
 
   // filtered mood data for mood chart
   const filteredMoodData = rawMoodData.map((item) => {
